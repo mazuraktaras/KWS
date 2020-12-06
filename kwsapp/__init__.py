244e3a4618c56910a729ac79f8b65c330c7bbaf0
mazuraktaras <mazuraktaras@users.noreply.github.com>
2020-12-06 17:02:00 +0200

    Alchemy added

--- kwsapp/__init__.py
+++ kwsapp/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+db = SQLAlchemy(app)
 
 # All that modules must be imported after app object created due Flask developers recommendation
 from kwsapp import views
