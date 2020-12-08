3aaf20e456c33359c968c35a55d94648ff59aafb
mazuraktaras <mazuraktaras@users.noreply.github.com>
2020-12-08 14:28:03 +0200

    Initial settings

--- kwsapp/models.py
+++ kwsapp/models.py
@@ -1,16 +1,16 @@
 from kwsapp import db
 
 
-# class User(db.Model):
-#     __tablename__ = 'users'
-#
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(128))
-#     password = db.Column(db.String(512))
-#     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
-#
-#     def __repr__(self):
-#         return '<User %r>' % self.username
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128))
+    password = db.Column(db.String(512))
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 
 class Role(db.Model):
@@ -24,26 +24,23 @@ class Role(db.Model):
         return '<Role %r>' % self.name
 
 
-class Post(db.Model):
-    id = db.Column(db.Integer(), primary_key=True)
-    title = db.Column(db.String(255))
-    text = db.Column(db.Text())
-    publish_date = db.Column(db.DateTime())
-    user_id = db.Column(db.Integer(), db.ForeignKey('user.id'))
-
-    def __init__(self, title):
-        self.title = title
-
-    def __repr__(self):
-        return "<Post '{}'>".format(self.title)
+# class Post(db.Model):
+#     id = db.Column(db.Integer(), primary_key=True)
+#     title = db.Column(db.String(255))
+#     text = db.Column(db.Text())
+#     publish_date = db.Column(db.DateTime())
+#     user_id = db.Column(db.Integer(), db.ForeignKey('user.id'))
+#
+#     def __repr__(self):
+#         return "<Post '{}'>".format(self.title)
 
 
-class User(db.Model):
-    id = db.Column(db.Integer(), primary_key=True)
-    username = db.Column(db.String(255))
-    password = db.Column(db.String(255))
-    posts = db.relationship(
-        'Post',
-        backref='user',
-        lazy='dynamic'
-    )
+# class User(db.Model):
+#     id = db.Column(db.Integer(), primary_key=True)
+#     username = db.Column(db.String(255))
+#     password = db.Column(db.String(255))
+#     posts = db.relationship(
+#         'Post',
+#         backref='user',
+#         lazy='dynamic'
+#     )
--- shell.py
+++ shell.py
@@ -1,6 +1,16 @@
 from kwsapp import app, db
 from kwsapp.models import User, Role
 
-users = User.query.all()
+db.create_all()
 
 
+def initial_settings():
+    admin_role = Role(name='admin')
+    user_role = Role(name='user')
+    guest_role = Role(name='guest')
+    inactive_role = Role(name='inactive')
+    db.session.add_all([admin_role, user_role, guest_role, inactive_role])
+    user = User(name='new@email.com')
+    user.role = user_role
+    db.session.add(user)
+    db.session.commit()
