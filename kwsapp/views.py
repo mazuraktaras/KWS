a36ae2b144c4bed4a75c9ef1c23adcf26678b5cd
mazuraktaras <53152688+mazuraktaras@users.noreply.github.com>
2020-12-16 22:46:43 +0200

    static created

--- kwsapp/views.py
+++ kwsapp/views.py
@@ -1,12 +1,12 @@
 from . import app, db
 from .models import User, Role
-from flask import render_template
+from flask import render_template, url_for
 from .resources import initial_settings, users_list
 
 
 @app.route('/')
 def hello_world():
     # initial_settings()
-
+    # print(url_for('static', filename='main.css'))
     return render_template('users_index.html', users=users_list())
     # return f'Hello World! {info}'
